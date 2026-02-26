407b4a6578c347c3db8021aa7ac0fb112c338426
venkateshpandrinki <venkateshpandrinki3@gmail.com>
2026-02-26 22:45:44 +0530

    middleware deprecated

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import { auth } from "@/auth"
 import { getTenantFromHost } from "@/utils/tenant"
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const res = NextResponse.next()
 
   const tenant = await getTenantFromHost(req.headers)
